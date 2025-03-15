b322822cf6adc4d4b7bf8d21bc1f3e465880fa22
Stuart Webster <sw_webster@hotmail.com>
2025-03-15 16:34:45 +0100

    feat: extract IOB and COB from devicestatus records

--- lib/overnight/nightscout/device_status.rb
+++ lib/overnight/nightscout/device_status.rb
@@ -8,7 +8,7 @@ module Overnight
   class Nightscout
     # the status of the devices uploading data to Nightscout, such as Loop or OpenAPS
     class DeviceStatus
-      attr_reader :timestamp, :predicted
+      attr_reader :timestamp, :iob, :cob, :predicted
 
       def self.request(token:, count: 1)
         Client.request('devicestatus', token:, params: { count: })
@@ -20,6 +20,9 @@ module Overnight
 
       def initialize(loop:)
         @timestamp = loop[:timestamp]
+        @iob = loop[:iob][:iob]
+        @cob = loop[:cob][:cob]
+
         predicted = loop[:predicted]
         start_date = predicted[:startDate]
 
