a603c5dfdca52f712e68452850b1d883870ef69d
Stuart Webster <sw_webster@hotmail.com>
2025-05-21 12:22:04 +0200

    test: replace setup in TestGlucoseRanges

--- test/overnight/nightscout/glucose_ranges_test.rb
+++ test/overnight/nightscout/glucose_ranges_test.rb
@@ -4,62 +4,59 @@ require 'minitest/autorun'
 require 'overnight/nightscout/status/glucose_ranges'
 
 class TestGlucoseRanges < Minitest::Test
-  def find(glucose)
-    @glucose_ranges.find(glucose)&.name
-  end
+  UPPER_BOUND_URGENT_LOW  =  56
+  UPPER_BOUND_LOW         =  81
+  LOWER_BOUND_HIGH        = 180
+  LOWER_BOUND_URGENT_HIGH = 247
 
-  def setup
-    @glucose_ranges = Overnight::Nightscout::Status::GlucoseRanges.new(
-      {
-        bgLow: low, bgTargetBottom: bottom, bgTargetTop: top, bgHigh: high
-      }
-    )
+  GLUCOSE_RANGES = Overnight::Nightscout::Status::GlucoseRanges.new({
+             bgLow: UPPER_BOUND_URGENT_LOW,
+    bgTargetBottom: UPPER_BOUND_LOW,
+       bgTargetTop: LOWER_BOUND_HIGH,
+            bgHigh: LOWER_BOUND_URGENT_HIGH
+  }).freeze
+
+  def find(glucose)
+    GLUCOSE_RANGES.find(glucose)&.name
   end
 
   def test_lower_bound_urgent_low
-    assert_equal :urgent_low, find(low - 0.6)
+    assert_equal :urgent_low, find(UPPER_BOUND_URGENT_LOW - 0.6)
   end
 
   def test_upper_bound_urgent_low
-    assert_equal :urgent_low, find(low + 0.4)
+    assert_equal :urgent_low, find(UPPER_BOUND_URGENT_LOW + 0.4)
   end
 
   def test_lower_bound_low
-    assert_equal :low, find(low + 0.6)
+    assert_equal :low, find(UPPER_BOUND_URGENT_LOW + 0.6)
   end
 
   def test_upper_bound_low
-    assert_equal :low, find(bottom + 0.4)
+    assert_equal :low, find(UPPER_BOUND_LOW + 0.4)
   end
 
   def test_lower_bound_normal
-    assert_equal :normal, find(bottom + 0.6)
+    assert_equal :normal, find(UPPER_BOUND_LOW + 0.6)
   end
 
   def test_upper_bound_normal
-    assert_equal :normal, find(top - 0.6)
+    assert_equal :normal, find(LOWER_BOUND_HIGH - 0.6)
   end
 
   def test_lower_bound_high
-    assert_equal :high, find(top - 0.4)
+    assert_equal :high, find(LOWER_BOUND_HIGH - 0.4)
   end
 
   def test_upper_bound_high
-    assert_equal :high, find(high - 0.6)
+    assert_equal :high, find(LOWER_BOUND_URGENT_HIGH - 0.6)
   end
 
   def test_lower_bound_urgent_high
-    assert_equal :urgent_high, find(high - 0.4)
+    assert_equal :urgent_high, find(LOWER_BOUND_URGENT_HIGH - 0.4)
   end
 
   def test_upper_bound_urgent_high
-    assert_equal :urgent_high, find(high + 0.6)
+    assert_equal :urgent_high, find(LOWER_BOUND_URGENT_HIGH + 0.6)
   end
-
-  private
-
-  def low = 56
-  def bottom = 81
-  def top = 180
-  def high = 247
 end
